f64acd68ed501aeab4b47c6a6e5d50f9957f5ec2
Casey Dunn <casey.dunn@yale.edu>
2020-04-27 23:15:48 -0400

    added my own attempt to read busy flag

--- 6502-monitor/6502-monitor.ino
+++ 6502-monitor/6502-monitor.ino
@@ -1,5 +1,9 @@
 const char ADDR[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
 const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
+
+const char PB[] = {23, 25, 27, 29, 31, 33, 35, 37};
+const char PA[] = {4, 5, 6, 7, 8, 9, 10, 11};
+
 #define CLOCK 2
 #define READ_WRITE 3
 
@@ -10,6 +14,14 @@ void setup() {
   for (int n = 0; n < 8; n += 1) {
     pinMode(DATA[n], INPUT);
   }
+
+  for (int n = 0; n < 8; n += 1) {
+    pinMode(PA[n], INPUT);
+  }
+    for (int n = 0; n < 8; n += 1) {
+    pinMode(PB[n], INPUT);
+  }
+  
   pinMode(CLOCK, INPUT);
   pinMode(READ_WRITE, INPUT);
 
@@ -20,6 +32,25 @@ void setup() {
 
 void onClock() {
   char output[15];
+
+  unsigned int pa = 0;
+  for (int n = 0; n < 8; n += 1) {
+    int bit = digitalRead(PA[n]) ? 1 : 0;
+    Serial.print(bit);
+    pa = (pa << 1) + bit;
+  }
+
+  Serial.print("   ");
+
+  unsigned int pb = 0;
+  for (int n = 0; n < 8; n += 1) {
+    int bit = digitalRead(PB[n]) ? 1 : 0;
+    Serial.print(bit);
+    pb = (pb << 1) + bit;
+  }
+
+  Serial.print("   ");
+
 
   unsigned int address = 0;
   for (int n = 0; n < 16; n += 1) {
---
+++ exploration/hello-world_post_vid08.s
@@ -0,0 +1,121 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+ACR = $600b
+
+
+E  = %10000000
+RW = %01000000
+RS = %00100000
+BF = %10000000 ; The busy flag
+
+  .org $8000
+
+reset:
+  ldx #$ff  ; a2 ff
+  txs       ; 9a
+
+  ; turn of data latching on Port A
+  ; http://lateblt.tripod.com/bit67.txt
+  lda #%00000000 ; a9 00 Setting last two bits (2 and 1) to 0 turns off latching for PA and PB input
+  sta ACR        ; 8d 0b 60
+
+  lda #%11111111 ; a9 ff    Set all pins on port B to output
+  sta DDRB       ; 8d 02 60
+
+  lda #%11100000 ; a9 e0     Set top 3 pins on port A to output
+  sta DDRA       ; 8d 03 60
+
+  lda #%00111000 ; a9 38  Set 8-bit mode; 2-line display; 5x8 font
+  jsr lcd_instruction ; 20 XX XX
+
+  lda #%00001110 ; Display on; cursor on; blink off
+  jsr lcd_instruction
+
+  lda #%00000110 ; Increment and shift cursor; don't shift display
+  jsr lcd_instruction
+
+  lda #%00000001 ; Clear Display
+  jsr lcd_instruction
+
+  lda #"H"
+  jsr print_char
+  lda #"e"
+  jsr print_char
+  lda #"l"
+  jsr print_char
+  lda #"l"
+  jsr print_char
+  lda #"o"
+  jsr print_char
+  lda #","
+  jsr print_char
+  lda #" "
+  jsr print_char
+  lda #"w"
+  jsr print_char
+  lda #"o"
+  jsr print_char
+  lda #"r"
+  jsr print_char
+  lda #"l"
+  jsr print_char
+  lda #"d"
+  jsr print_char
+  lda #"!"
+  jsr print_char
+
+loop:
+  jmp loop
+
+
+check_busy:
+
+  lda #%00000000 ; a9 00       Set all pins on port B to input
+  sta DDRB       ; 8d 02 60
+
+  lda #RW        ; a9 40       Set just R bit to read busy flag
+  sta PORTA      ; 8d 01 60
+
+while_busy:
+  lda PORTB      ; ad 00 60       Read the busy flag
+  eor #%00000000 ; 49 00       Check if it is zero
+  and #BF        ; 29 80       Select only the relevent bit
+  bne while_busy ; d0          If result isn't zero, try again
+
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+
+  lda #%11111111 ; Set all pins on port B to output
+  sta DDRB
+
+  rts
+
+
+lcd_instruction:
+  jsr check_busy     ; 20 XX XX   wait until the lcd isn't busy
+  sta PORTB
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  lda #E         ; Set E bit to send instruction
+  sta PORTA
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+print_char:
+  jsr check_busy     ; wait until the lcd isn't busy
+  sta PORTB
+  lda #RS         ; Set RS; Clear RW/E bits
+  sta PORTA
+  lda #(RS | E)   ; Set E bit to send instruction
+  sta PORTA
+  lda #RS         ; Clear E bits
+  sta PORTA
+  rts
+
+
+  .org $fffc
+  .word reset
+  .word $0000
