5e6f221e7597c676b839795b09e2e73fa7eb9a45
Casey Dunn <casey.dunn@yale.edu>
2020-05-04 21:30:45 -0400

    copied final program from eater.net

--- hello-world_vid09.s
+++ hello-world_vid09.s
@@ -15,53 +15,53 @@ reset:
 
   lda #%11111111 ; Set all pins on port B to output
   sta DDRB
-
   lda #%11100000 ; Set top 3 pins on port A to output
   sta DDRA
 
   lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
   jsr lcd_instruction
-
   lda #%00001110 ; Display on; cursor on; blink off
   jsr lcd_instruction
-
   lda #%00000110 ; Increment and shift cursor; don't shift display
   jsr lcd_instruction
+  lda #$00000001 ; Clear display
+  jsr lcd_instruction
 
-  lda #%00000001 ; Clear Display
-  jsr lcd_instruction 
-
-  lda #"H"
+  ldx #0
+print:
+  lda message,x
+  beq loop
   jsr print_char
-  lda #"e"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"o"
-  jsr print_char
-  lda #","
-  jsr print_char
-  lda #" "
-  jsr print_char
-  lda #"w"
-  jsr print_char
-  lda #"o"
-  jsr print_char
-  lda #"r"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"d"
-  jsr print_char
-  lda #"!"
-  jsr print_char
+  inx
+  jmp print
 
 loop:
   jmp loop
 
+message: .asciiz "Hello, world!"
+
+lcd_wait:
+  pha
+  lda #%00000000  ; Port B is input
+  sta DDRB
+lcdbusy:
+  lda #RW
+  sta PORTA
+  lda #(RW | E)
+  sta PORTA
+  lda PORTB
+  and #%10000000
+  bne lcdbusy
+
+  lda #RW
+  sta PORTA
+  lda #%11111111  ; Port B is output
+  sta DDRB
+  pla
+  rts
+
 lcd_instruction:
+  jsr lcd_wait
   sta PORTB
   lda #0         ; Clear RS/RW/E bits
   sta PORTA
@@ -72,6 +72,7 @@ lcd_instruction:
   rts
 
 print_char:
+  jsr lcd_wait
   sta PORTB
   lda #RS         ; Set RS; Clear RW/E bits
   sta PORTA
@@ -80,7 +81,6 @@ print_char:
   lda #RS         ; Clear E bits
   sta PORTA
   rts
-
 
   .org $fffc
   .word reset
