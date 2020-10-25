3e4f9b72d62146df79dba9b1c5c14618f29f8262
Casey Dunn <casey.dunn@yale.edu>
2020-10-24 22:31:36 -0400

    video 10 code works

--- hello-world_vid10.s
+++ hello-world_vid10.s
@@ -3,6 +3,11 @@ PORTA = $6001
 DDRB = $6002
 DDRA = $6003
 
+value = $0200 ; 2 bytes
+mod10 = $0202 ; 2 bytes
+message = $0204 ; 6 bytes
+
+
 E  = %10000000
 RW = %01000000
 RS = %00100000
@@ -26,14 +31,92 @@ reset:
   jsr lcd_instruction
   lda #$00000001 ; Clear display
   jsr lcd_instruction
+  
+  lda #0
+  sta message
 
-  lda #"0" + %0100
+  ; Initialize vale to be the number to convert
+  lda number
+  sta value
+  lda number + 1
+  sta value + 1
+
+divide:
+  ; Initialize the remainder to zero
+  lda #0
+  sta mod10
+  sta mod10 + 1
+  clc
+
+  ldx #16
+
+divloop:
+  ; Rotate quotient and remainder
+  rol value
+  rol value + 1
+  rol mod10
+  rol mod10 + 1
+
+  ; a,y = dividend - devisor
+  sec
+  lda mod10
+  sbc #10
+  tay ; save low byte in Y
+  lda mod10+1
+  sbc #0
+  bcc ignore_result ; branch if dividend < devisor
+  sty mod10
+  sta mod10 + 1
+
+ignore_result:
+  dex
+  bne divloop
+  rol value ; shift in the last bit of the quotient
+  rol value + 1
+
+  lda mod10
+  clc
+  adc #"0"
+  jsr  push_char
+
+  ; if value != 0, then continue dividing
+  lda value
+  ora value + 1
+  bne divide ; branch if value not equal to 0
+
+  ldx #0
+print:
+  lda message,x
+  beq loop
   jsr print_char
-  lda #"0" + %0010
-  jsr print_char
+  inx
+  jmp print
 
 loop:
   jmp loop
+
+number: .word 1729
+
+; Add the character in the A register to the beginning of the 
+; null-terminated string `message`
+push_char:
+  pha ; Push new first char onto stack
+  ldy #0
+
+char_loop:
+  lda message,y ; Get char on the string and put into X
+  tax
+  pla
+  sta message,y ; Pull char off stack and add it to the string
+  iny
+  txa
+  pha           ; Push char from string onto stack
+  bne char_loop
+
+  pla
+  sta message,y ; Pull the null off the stack and add to the end of the string
+
+  rts
 
 lcd_wait:
   pha
